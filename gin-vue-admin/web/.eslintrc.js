f0500e68e93688813d7e7d9321022c0a8554dabb
lazyou <1394139539@qq.com>
2025-10-26 13:15:01 +0800

    gva: 尝试vscode配置任务、eslit、代码片段等，尚未看到配置效果

---
+++ gin-vue-admin/web/.eslintrc.js
@@ -0,0 +1,11 @@
+{
+  "typescript.preferences.importModuleSpecifier": "relative",
+  "typescript.suggest.autoImports": true,
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  },
+  "eslint.workingDirectories": ["web"],
+  "vetur.validation.template": false,
+  "vetur.validation.script": false,
+  "vetur.validation.style": false
+}
